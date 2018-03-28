c3fa31f605e49dd6ed57d7ecafb621ac6b485ef5
Daogaru Cristina <cristina.florentina.daogaru@gmail.com>
2018-03-28 23:58:11 +0300

    get the positive and negative documents reviews from files reviews

--- FeedbackAnalysis/main.py
+++ FeedbackAnalysis/main.py
@@ -1,8 +1,27 @@
+from Helpers.DocumentHandler import DocumentHandler
 
 
 def main():
 
-    return
+    # TODO : Refactor DocumentHandler class based on the fallowing code
+
+    documentHandler = DocumentHandler()
+
+    shortPositiveReviewsPath = "short_reviews/positive.txt"
+    shortNegativeReviewsPath = "short_reviews/negative.txt"
+
+    positiveDocumentsReview = documentHandler.GetPositiveDocumets(shortPositiveReviewsPath, "r")
+    negativeDocumentsReview = documentHandler.GetNegativeDocumets(shortNegativeReviewsPath, "r")
+
+    documents = []
+
+    for r in positiveDocumentsReview.split('\n'):
+        documents.append((r, "pos"))
+
+    for r in negativeDocumentsReview.split('\n'):
+        documents.append((r, "neg"))
+
+
 
 if __name__ == '__main__':
     main()
