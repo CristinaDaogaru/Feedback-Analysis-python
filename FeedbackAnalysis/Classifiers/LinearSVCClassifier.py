cd0b742b9e9c18f16b0be378215e7a9f98d98092
Daogaru Cristina <cristina.florentina.daogaru@gmail.com>
2018-03-28 23:25:07 +0300

    added liniar svc classifier wrapper

---
+++ FeedbackAnalysis/Classifiers/LinearSVCClassifier.py
@@ -0,0 +1,24 @@
+import nltk
+from nltk.classify.scikitlearn import SklearnClassifier
+from sklearn.svm import LinearSVC
+
+
+class LinearSVCClassifier:
+
+    def __init__(self):
+
+        self.classifier = SklearnClassifier(LinearSVC())
+
+
+    def Train(self, trainingSet):
+
+        self.classifier.train(trainingSet)
+
+
+    def Accuracy(self, testingSet):
+
+        return (nltk.classify.accuracy(self.classifier, testingSet)) * 100
+
+
+
+
