8d9604390fd8b8c3d78affa073d5d91ed572494e
Daogaru Cristina <cristina.florentina.daogaru@gmail.com>
2018-08-05 21:28:08 +0300

    generate training data from data files

---
+++ FeedbackAnalysis/Helpers/TrainingDataCreator.py
@@ -0,0 +1,15 @@
+from Helpers.FileReader import FileReader
+
+
+class TrainingDataCreator:
+
+    @staticmethod
+    def Create(filePath, reviewClass, numberOfElements):
+        trainingData = []
+        fileReader = FileReader()
+        positiveWords = fileReader.ReadLines(filePath)
+
+        for index in range(0, numberOfElements):
+            trainingData.append({"class": reviewClass, "sentence": positiveWords[index]})
+
+        return trainingData
