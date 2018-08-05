0679a924b27aa971a3502af7270fec5e2b67018b
Daogaru Cristina <cristina.florentina.daogaru@gmail.com>
2018-08-05 16:48:16 +0300

    Import web project

---
+++ FeedBackPlatformWeb/FeedBackPlatformWeb/Database/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using FeedBackPlatformWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FeedBackPlatformWeb.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("DatabaseContext"){}
+        public DbSet<Survey> Surveys { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
